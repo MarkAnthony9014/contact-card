523997f0337c58fceb730b2d9504aad58f6dd671
MarkAnthony9014 <markanthony9014@gmail.com>
2022-11-17 16:09:04 -0600

    added heroku script to package json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,50 +1,42 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
-
-  module.exports = {
-    entry: './src/js/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-      ]
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', { targets: "defaults" }]
-              ]
-            }
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
           }
         }
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin',
-      })
+      }
+
     ]
-  };
+  }
+};
+
--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,8 +1,7 @@
 const path = require('path');
 
-//this allow HTML to be served up by the server.
 module.exports = function(app) {
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../client/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 };
