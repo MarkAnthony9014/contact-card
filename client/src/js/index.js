30303a1cb223de76feebe8e88d82bbe670e02864
MarkAnthony9014 <markanthony9014@gmail.com>
2022-11-17 23:58:23 -0600

    added database js file

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,7 +2,7 @@ import "./form";
 import "./submit";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initdb } from './database';
+import { initdb, getDb, postDb } from './database';
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
@@ -10,6 +10,7 @@ import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
     initdb();
+    getDb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
